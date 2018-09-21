e6bdb311310bd5e7c3557de8f5163cb85733b5f1
zebnat <zebnat@gmail.com>
2018-09-22 01:56:46 +0200

    ADD Translations for SkillPage

--- data/translations/skillsPage.js
+++ data/translations/skillsPage.js
@@ -3,15 +3,38 @@ const translation = {
     h1: 'My Skills IT',
     h2: `Technologies i've used and comfort level`,
     title: 'IT skills and programming languages - Daniel Domínguez',
-		quote: `asdf`,
-		wordLevel: 'Level'
+		wordLevel: 'Level',
+		levels_explained: 'Levels guide',
+		help_l1: 'Basic knowledge and theory about the tool.',
+		help_l2: 'The tool has been used for practice.',
+		help_l3: 'The tool has been used in real proyects.',
+		help_l4: 'Used for years, decent skill level for the most use cases.',
+		help_l5: 'Big confidence and advanced knowledge, years of experience.',
+		blinking: 'Blinking',
+		help_blinking: 'Used recently, for the last 2 years.',
+		languages: 'Programming languages',
+		frameworks: 'Frameworks / Libs / CMS',
+		other_tools: 'Other tools or skills',
+		future_skills: 'Want to learn for the future'
   },
   es: {
     h1: 'Dominio de Tecnologías IT',
     h2: 'Que tecnologías he utilizado y el nivel de comfort',
     title: 'Dominio de tecnologías y lenguajes - Daniel Domínguez',
-		quote: `asdf`,
-		wordLevel: 'Nivel'
+		wordLevel: 'Nivel',
+		levels_explained: 'Guía de niveles',
+		help_l1: 'Conocimiento teórico básico sobre la herramienta',
+		help_l2: 'Se ha practicado con la herramienta para comprenderla mejor.',
+		help_l3: 'Se ha utilizado en proyectos reales con problemas reales.',
+		help_l4: 'Se ha utilizado durante años, se conoce lo suficiente para trabajar con soltura.',
+		help_l5: 'Se tiene gran confianza y conocimiento avanzado, además de años de experiencia.',
+		blinking: 'Parpadeo',
+		help_blinking: 'Se ha utilizado de recientemente, al menos en los últimos 2 años.',
+		languages: 'Lenguajes de programación',
+		frameworks: 'Frameworks / Librerías / CMS',
+		other_tools: 'Otras herramientas o técnicas',
+		future_skills: 'Me interesan para el futuro'
+
   }
 }
 
