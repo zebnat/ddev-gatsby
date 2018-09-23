eacb31bd2c28aed5fb2b2c8fbe196621ea202251
zebnat <zebnat@gmail.com>
2018-09-23 20:33:57 +0200

    ADD added translations for footer info

---
+++ data/translations/footer.js
@@ -0,0 +1,16 @@
+const translation = {
+  en: {
+    contact: 'Contact Info',
+    contactInfo: 'Do you need a developer like me? If your company is located at the Barcelonés Area (Barcelona), do not think twice, contact me :). I leave different contact options below.',
+		warning: 'Warning',
+		warningInfo: 'At this moment i can work only in the afternoons (after 15:00), i am studying on mornings.',
+  },
+  es: {
+    contact: 'Vías de Contacto',
+    contactInfo: '¿Necesitas un programador con mi perfil? Si tu empresa está situada sobre el area del Barcelonés (Barcelona) no dudes en ponerte en contacto conmigo. Aquí te dejo varias opciones para contactar conmigo.',
+		warning: 'Atención',
+		warningInfo: 'Mi disponibilidad para trabajar en estos momentos está limitada a las tardes, estoy cursando unos estudios por la mañana. Gracias.',
+  },
+}
+
+export default translation
