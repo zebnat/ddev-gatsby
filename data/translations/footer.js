716829a58464c8d309a66d357bc4fb4eafca9c12
zebnat <zebnat@gmail.com>
2018-11-16 17:00:05 +0100

    WIP small changes to text content

--- data/translations/footer.js
+++ data/translations/footer.js
@@ -2,19 +2,19 @@ const translation = {
   en: {
     contact: 'Contact Me',
     contactInfo:
-      'Do you have any questions? Are you interested in hiring? I can work around the Barcelonés area (Barcelona). I leave different contact options below.',
+      'Do you have any questions? Are you interested in hiring? I work around the Barcelonés area (Barcelona). I leave different contact options below.',
     warning: 'Attention',
-    warningInfo: `I'm currently studying. My availability is limited to 16:00 in advance. If you need a part-time web developer for the afternoon, contact me.`,
-    developedBy: `This Progressive Web App was made by <b>Daniel Domínguez Rubio</b> (@zebnat) using technologies like ReactJS GatsbyJS GraphQL`,
+    warningInfo: `I'm currently studying. My availability is limited to 16:00 in advance. If you need a part-time web developer for the afternoon, contact me. This will be like this until i finish studying.`,
+    developedBy: `This Progressive Web App was made by <b>Daniel Domínguez Rubio</b> (@zebnat) using technologies like ReactJS GatsbyJS GraphQL NodeJS`,
   },
   es: {
     contact: 'Vías de Contacto',
     contactInfo:
-      '¿Alguna pregunta? ¿Te gustaría entrevistarme para un empleo? Suelo trabajar en el area del Barcelonés (Barcelona). Abajo dejo varias opciones para contactar.',
+      '¿Alguna pregunta? ¿Te gustaría hablar para un empleo? Suelo trabajar por el Barcelonés (Barcelona). Abajo dejo varias opciones para contactar conmigo.',
     warning: 'Atención',
     warningInfo:
-      'En este momento estoy sacándome un FP Superior, mi disponibilidad está limitada a las 16:00 y adelante. Si necesitas un programador web con experiencia para media jornada de tarde, contacta.',
-    developedBy: `Esta Aplicación Web Progresiva (PWA) ha sido desarrollada por <b>Daniel Domínguez Rubio</b> (@zebnat) usando tecnologías como ReactJS GatsbyJS GraphQL`,
+      'En este momento estoy sacándome un FP Superior. Mi disponibilidad está limitada a las 16:00 y en adelante. Si necesitas un programador web con experiencia para media jornada de tarde, contacta. Esta limitación acabará en cuanto obtenga el título de FP.',
+    developedBy: `Esta Aplicación Web Progresiva (PWA) ha sido desarrollada por <b>Daniel Domínguez Rubio</b> (@zebnat) usando tecnologías como ReactJS GatsbyJS GraphQL NodeJS`,
   },
 }
 
