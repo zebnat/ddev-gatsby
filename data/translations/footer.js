8d9b492705c36821c51fb181e8d58716641f5e46
zebnat <zebnat@gmail.com>
2018-11-24 18:09:23 +0100

    WIP content added

--- data/translations/footer.js
+++ data/translations/footer.js
@@ -2,7 +2,7 @@ const translation = {
   en: {
     contact: 'Contact Me',
     contactInfo:
-      'Do you have any questions? Are you interested in hiring me? I work around the Barcelonés area (Barcelona). I leave different contact options below.',
+      'Do you have any questions? Want to talk about a job offer? I work around the Barcelonés area (Barcelona). I leave different contact options below.',
     warning: 'Attention',
     warningInfo: `I'm currently studying. My availability is limited to 16:00 in advance. If you need a part-time web developer for the afternoon, contact me. This will be like this until i finish studying.`,
     developedBy: `This Progressive Web App was made by <b>Daniel Domínguez Rubio</b> (@zebnat) using technologies like ReactJS GatsbyJS GraphQL NodeJS`,
@@ -10,7 +10,7 @@ const translation = {
   es: {
     contact: 'Vías de Contacto',
     contactInfo:
-      '¿Alguna pregunta? ¿Te gustaría hablar para un empleo? Suelo trabajar por el Barcelonés (Barcelona). Abajo dejo varias opciones para contactar conmigo.',
+      '¿Alguna pregunta? ¿Te gustaría hablar para una oferta laboral? Suelo trabajar por el Barcelonés (Barcelona). Abajo dejo varias opciones para contactar conmigo.',
     warning: 'Atención',
     warningInfo:
       'En este momento estoy sacándome un FP Superior. Mi disponibilidad está limitada a las 16:00 y en adelante. Si necesitas un programador web con experiencia para media jornada de tarde, contacta. Esta limitación acabará en cuanto obtenga el título de FP.',
