382bce0a3fe8e9fe4a72e49ac6efb42137254caf
zebnat <zebnat@gmail.com>
2019-02-02 21:45:01 +0100

    FIX edited footer template page text

--- data/translations/footer.js
+++ data/translations/footer.js
@@ -4,7 +4,7 @@ const translation = {
     contactInfo:
       'Do you have any questions? Want to talk about a job offer? I work around the Barcelonés area (Barcelona). I leave different contact options below.',
     warning: 'Attention',
-    warningInfo: `I'm currently studying. My availability is limited to 16:00 in advance. If you need a part-time web developer for the afternoon, contact me. This will be like this until i finish studying.`,
+    warningInfo: `I'm currently studying. My availability is limited to part-time. If you need a part-time web developer, get in touch.`,
     developedBy: `This Progressive Web App was made by <b>Daniel Domínguez Rubio</b> (@zebnat) using technologies like ReactJS GatsbyJS GraphQL NodeJS`,
   },
   es: {
@@ -13,8 +13,8 @@ const translation = {
       '¿Alguna pregunta? ¿Te gustaría hablar para una oferta laboral? Suelo trabajar por el Barcelonés (Barcelona). Abajo dejo varias opciones para contactar conmigo.',
     warning: 'Atención',
     warningInfo:
-      'En este momento estoy sacándome un FP Superior. Mi disponibilidad está limitada a las 16:00 y en adelante. Si necesitas un programador web con experiencia para media jornada de tarde, contacta. Esta limitación acabará en cuanto obtenga el título de FP.',
-    developedBy: `Esta Aplicación Web Progresiva (PWA) ha sido desarrollada por <b>Daniel Domínguez Rubio</b> (@zebnat) usando tecnologías como ReactJS GatsbyJS GraphQL NodeJS`,
+      'En este momento estoy ampliando mi formación. Mi disponibilidad está limitada a media jornada. Si necesitas un programador web con experiencia para media jornada, no dudes en contactar.',
+    developedBy: `Esta PWA ha sido desarrollada por <b>Daniel Domínguez Rubio</b> usando tecnologías como ReactJS GatsbyJS GraphQL NodeJS`,
   },
 }
 
