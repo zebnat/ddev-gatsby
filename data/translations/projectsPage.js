6456e4c95373658f78cb170662fab1507032101f
zebnat <zebnat@gmail.com>
2019-06-08 21:43:07 +0200

    FIX modified translation text

--- data/translations/projectsPage.js
+++ data/translations/projectsPage.js
@@ -4,14 +4,14 @@ const translation = {
     h1: 'My Projects',
     h2: `Professional Work & Personal Projects`,
 		readmore: `Show Content`,
-		summary: `Some of the projects that I made or was involved with. Some of them are personal projects and others are the ones I did while working for a company, with their permission to share.`,
+		summary: `Some of the projects that I made or was involved with. Some of them are personal projects and others are the ones I did while working for a company.`,
   },
   es: {
     title: 'Mis Trabajos y proyectos - Daniel Domínguez',
     h1: 'Proyectos y trabajos',
 		h2: 'Proyectos personales y para empresas',
 		readmore: `Ver contenido`,
-		summary: `Proyectos que hice yo mismo o con más compañeros. Algunos son personales, otros los programé para empresas, con el consentimiento de éstas para compartirlo aquí.`,
+		summary: `Algunos de los proyectos más relevantes que hice yo mismo o con más compañeros. Hay proyectos personales y también trabajos hechos para empresas.`,
   },
 }
 
