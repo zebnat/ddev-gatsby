4c3ee637ca20f3fe7bf0c752ce8c42b02a4a2b16
zebnat <zebnat@gmail.com>
2019-09-22 16:00:32 +0200

    fix nginx allow downloading of sh and cfg files

--- static/scripts/vimsetup.sh
+++ static/scripts/vimsetup.sh
@@ -2,4 +2,4 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-curl -fLo ~/.vimrc https://www.danieldev.es/scripts/myvimrc
+curl -fLo ~/.vimrc https://www.danieldev.es/scripts/vimrc.cfg
