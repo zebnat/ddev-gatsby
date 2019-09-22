03c96d4d37baaa17d7a21800ac16569d2c471d95
zebnat <zebnat@gmail.com>
2019-09-22 16:07:43 +0200

    FIX added colorprofiles to vimsetup

--- static/scripts/vimsetup.sh
+++ static/scripts/vimsetup.sh
@@ -3,3 +3,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 curl -fLo ~/.vimrc https://www.danieldev.es/scripts/vimrc.cfg
+
+curl -fLo ~/.vim/colors/atom-dark-256.vim https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark-256.vim
+curl -fLo ~/.vim/colors/atom-dark.vim https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark.vim
