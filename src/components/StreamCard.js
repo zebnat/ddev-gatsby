06676b336cd062cc0947640dbe64daabd83995d9
zebnat <zebnat@gmail.com>
2021-08-30 02:51:35 +0200

    feat: improvements to streamcard
    now displays next stream with also days
    better css styling and even more cute

--- src/components/StreamCard.js
+++ src/components/StreamCard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import translation from '../../data/translations/streamerPage'
 import { Helmet } from 'react-helmet'
+import cuteImg from '../../data/images/875557672919896105.png'
 
 const StreamCard = (props) => {
 
@@ -16,12 +17,12 @@ const StreamCard = (props) => {
             .then(response => response.json())
             .then(data => {
                 setIsLive(data.live)
-                
-                if(data.program !== undefined) {
+
+                if (data.program !== undefined) {
                     setPlan(data.program)
                 }
 
-                if(data.timeleft) {
+                if (data.timeleft) {
                     setInterval(() => {
                         setTimeLeft(when(data.timeleft))
                     }, 1000)
@@ -33,37 +34,37 @@ const StreamCard = (props) => {
         {
             maxViewers: 20,
             reached: true,
-            message: translation[props.lang].done
+            message: null
         },
         {
             maxViewers: 40,
             reached: true,
-            message: translation[props.lang].done
+            message: null
         },
         {
             maxViewers: 60,
-            reached: true,
-            message: translation[props.lang].reachable
+            reached: false,
+            message: translation[props.lang].newModerator
         },
         {
             maxViewers: 80,
-            reached: true,
+            reached: false,
             message: translation[props.lang].newModerator
         },
         {
             maxViewers: 100,
-            reached: true,
+            reached: false,
             message: translation[props.lang].newArt
         },
 
     ]
 
     const when = (dt) => {
-        var utcDate = new Date(dt).getTime();      
+        var utcDate = new Date(dt).getTime();
         var now = new Date().getTime();
         var distance = utcDate - now;
 
-        if(distance <= 0) {
+        if (distance <= 0) {
             return "???";
         }
         return msToTime(utcDate - now);
@@ -79,17 +80,17 @@ const StreamCard = (props) => {
                 {isLive === true && <h1 css={streamStatus} dangerouslySetInnerHTML={{ __html: translation[props.lang].isLive }}></h1>}
                 {isLive === false && <h1 css={streamStatus} dangerouslySetInnerHTML={{ __html: translation[props.lang].isNotLive }}></h1>}
                 {isLive === null && <h1 css={streamStatus} dangerouslySetInnerHTML={{ __html: translation[props.lang].checkIsLive }}></h1>}
-                
+
                 <a href="https://www.twitch.tv/zebnat"><img css={{ margin: 0 }} src="https://zebnat.github.io/twitchtag.png" /></a>
                 {isLive && <a dangerouslySetInnerHTML={{ __html: translation[props.lang].visitStream }} css={actionButton} href="https://www.twitch.tv/zebnat"></a>}
-                {!isLive && <div>Siguiente Stream: {timeLeft}</div>}
-                {plan && <h3 onClick={() => { setProgramOpen(!programOpen) }}>{translation[props.lang].program} {programOpen ? `👇` : `👈`}</h3>}
+                {!isLive && <div>Siguiente Stream en...<br></br> <span css={{ color: "#dfceff", textShadow: '1px 2px #2c1556' }}>{timeLeft}</span></div>}
+                {plan && <h3 css={dropDownable} onClick={() => { setProgramOpen(!programOpen) }}>{programOpen ? `👇` : `👉`} {translation[props.lang].program} {programOpen ? `👇` : `👈`}</h3>}
                 {plan && <div style={programOpen ? { display: "block" } : { display: "none" }}>
-                    {plan.map((e, i) => <div key={i} css={programBlock}><h5>#{i+1} {e.title}</h5><p>{e.description}</p></div>)}
+                    {plan.map((e, i) => <div key={i} css={programBlock}><h5>Bloque #{i + 1}: {e.title}</h5><p>{e.description}</p></div>)}
                 </div>}
-                <h3 onClick={() => { setGoalsOpen(!goalsOpen) }}>{translation[props.lang].goals} {goalsOpen ? `👇` : `👈`}</h3>
+                <h3 css={dropDownable} onClick={() => { setGoalsOpen(!goalsOpen) }}>{goalsOpen ? `👇` : `👉`} {translation[props.lang].goals} {goalsOpen ? `👇` : `👈`}</h3>
                 <div css={goalsBox} style={goalsOpen ? { display: "block" } : { display: "none" }}>
-                    {goals.map((e, i) => <div key={i} css={{ fontSize: "80%" }}>[{e.reached ? '✓' : '✖'}] <span>{e.maxViewers} {translation[props.lang].visits}</span> ⇨ {e.message}</div>)}
+                    {goals.map((e, i) => <div key={i} css={programBlock}><h5>[{e.reached ? '✓' : '✖'}] {e.maxViewers} {translation[props.lang].visits}</h5>{e.reached && <p css={{ color: "#12fb12", display: "flex", justifyContent: "center", alignItems: "center" }}>{translation[props.lang].done} <img aria-label=":Jijis:" src={cuteImg} alt=":Jijis:" draggable="false"></img></p>} <p css={{ padding: "2 5" }}>{e.message}</p></div>)}
                 </div>
             </div>
         </>
@@ -100,25 +101,32 @@ export default StreamCard
 
 function msToTime(duration) {
     var milliseconds = Math.floor((duration % 1000) / 100),
-      seconds = Math.floor((duration / 1000) % 60),
-      minutes = Math.floor((duration / (1000 * 60)) % 60),
-      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-  
+        seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24),
+        days = Math.floor((duration / (1000 * 60 * 60 * 24)));
+
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
-  
-    return hours + ":" + minutes + ":" + seconds;
-  }
+
+    return (days > 0 ? `${days}d ` : ``) + hours + ":" + minutes + ":" + seconds;
+}
 
 // COMPONENT STYLES
+
+const dropDownable = {
+    color: "#e2d7f7",
+    cursor: "pointer"
+}
 
 const programBlock = {
     display: "flex",
     flexDirection: "column",
     justifyContent: "left",
     background: "#7b55c1",
-    padding: 5
+    padding: 5,
+    borderBottom: "2px dotted #6441a4"
 }
 const mainCard = {
     textAlign: "center",
